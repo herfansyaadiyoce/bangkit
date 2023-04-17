5890593d8d10dcc08fae0386f54b07218d49fd23
Roy <roypra272@gmail.com>
2023-04-17 22:26:08 +0700

    oke

--- login.php
+++ login.php
@@ -7,7 +7,7 @@ error_reporting(0);
 session_start();
  
 if (isset($_SESSION['username'])) {
-    header("Location: index.html");
+    header("Location: index.php");
 }
  
 if (isset($_POST['submit'])) {
@@ -19,7 +19,7 @@ if (isset($_POST['submit'])) {
     if ($result->num_rows > 0) {
         $row = mysqli_fetch_assoc($result);
         $_SESSION['username'] = $row['username'];
-        header("Location: index.html");
+        header("Location: index.php");
     } else {
         echo "<script>alert('username atau password Anda salah. Silahkan coba lagi!')</script>";
     }
