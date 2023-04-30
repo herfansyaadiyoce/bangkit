11e5304d3aa8698b5740649eaa4851b733c6caf8
herfansyaadiyoce <herfansyaadiyoce@gmail.com>
2023-05-01 00:06:12 +0700

    edit okeh

---
+++ bacalogin.php
@@ -0,0 +1,37 @@
+<?php
+
+$Option = array(
+    "ssl" =>array(
+        "verify_peer" => false,
+        "verify_peer_name"=>false,
+    ),
+);
+
+// Tiga link API JSON
+$link1 = "https://bangkit-caf86-default-rtdb.firebaseio.com/users.json";
+
+// Mengambil data dari setiap link API JSON
+$dataApi1 = file_get_contents($link1, false, stream_context_create($Option));
+
+
+// Mendekode setiap data JSON menjadi array PHP
+$data1 = json_decode($dataApi1, true);
+
+
+// // Menampilkan setiap data dalam format yang mudah dibaca
+// echo "<h3>Data Layanan</h3>";
+// echo "<pre>";
+// print_r($data1);
+// echo "</pre>";
+
+// echo "<h3>Data Testimoni</h3>";
+// echo "<pre>";
+// print_r($data2);
+// echo "</pre>";
+
+// echo "<h3>Data Customer</h3>";
+// echo "<pre>";
+// print_r($data3);
+// echo "</pre>";
+
+?>
--- login.php
+++ login.php
@@ -1,29 +1,48 @@
 <?php 
- 
-include 'config.php';
- 
-error_reporting(0);
- 
-session_start();
- 
-if (isset($_SESSION['username'])) {
-    header("Location: index.php");
-}
- 
-if (isset($_POST['submit'])) {
+
+require_once"init.php";
+require_once"lihatdata.php";
+
+ if (isset($_POST['submit'])) {
     $username = $_POST['username'];
-    $password = md5($_POST['password']);
- 
-    $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
-    $result = mysqli_query($conn, $sql);
-    if ($result->num_rows > 0) {
-        $row = mysqli_fetch_assoc($result);
-        $_SESSION['username'] = $row['username'];
-        header("Location: index.php");
+    $password = $_POST['password'];
+
+    if (!empty(trim($password)) && !empty(trim($username))) {
+        // if (login_cek_email($username)) {
+        //     cek_data($username, $password);
+        // } else {
+        //     echo "Email belum terdaftar";
+        // }
+        cek_data($username, $password);
     } else {
-        echo "<script>alert('username atau password Anda salah. Silahkan coba lagi!')</script>";
+        echo "Sorry it can't be empty";
     }
 }
+
+// include 'config.php';
+ 
+// error_reporting(0);
+ 
+// session_start();
+ 
+// if (isset($_SESSION['username'])) {
+//     header("Location: index.php");
+// }
+ 
+// if (isset($_POST['submit'])) {
+//     $username = $_POST['username'];
+//     $password = md5($_POST['password']);
+ 
+//     $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+//     $result = mysqli_query($conn, $sql);
+//     if ($result->num_rows > 0) {
+//         $row = mysqli_fetch_assoc($result);
+//         $_SESSION['username'] = $row['username'];
+//         header("Location: index.php");
+//     } else {
+//         echo "<script>alert('username atau password Anda salah. Silahkan coba lagi!')</script>";
+//     }
+// }
  
 ?>
  
@@ -46,12 +65,12 @@ if (isset($_POST['submit'])) {
                 <hr>
                 <div class="form-group">
                     <label>Username : </label>
-                    <input type="username" name="username" class="ïnput-control" placeholder="Masukan Username" value="<?php echo $username; ?>" required>
+                    <input type="username" name="username" class="ïnput-control" placeholder="Masukan Username" required>
                 </div>
 
                 <div class="form-group">
                     <label>Password : </label>
-                    <input type="password" name="password" placeholder="Masukan Password" class="ïnput-control" value="<?php echo $_POST['password']; ?>" required>
+                    <input type="password" name="password" placeholder="Masukan Password" class="ïnput-control"  required>
                 </div>
                 <input type="submit"name="submit" value="LOGIN" class="btn">
                 <p>Don't have an account? <a href="register.php">Sign Up</a> </p>
