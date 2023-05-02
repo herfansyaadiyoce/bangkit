f71d10a1efcf626b45cbd443da04d549049eaab4
herfansyaadiyoce <herfansyaadiyoce@gmail.com>
2023-05-02 15:43:04 +0700

    edit

--- bacalogin.php
+++ bacalogin.php
@@ -17,21 +17,4 @@ $dataApi1 = file_get_contents($link1, false, stream_context_create($Option));
 // Mendekode setiap data JSON menjadi array PHP
 $data1 = json_decode($dataApi1, true);
 
-
-// // Menampilkan setiap data dalam format yang mudah dibaca
-// echo "<h3>Data Layanan</h3>";
-// echo "<pre>";
-// print_r($data1);
-// echo "</pre>";
-
-// echo "<h3>Data Testimoni</h3>";
-// echo "<pre>";
-// print_r($data2);
-// echo "</pre>";
-
-// echo "<h3>Data Customer</h3>";
-// echo "<pre>";
-// print_r($data3);
-// echo "</pre>";
-
 ?>
--- lihatdata.php
+++ lihatdata.php
@@ -1,28 +1,5 @@
 <?php
-function cek_data($username, $password){
-    // global $conn;
-    // $username = mysqli_real_escape_string($conn, $username);
-    // $password = mysqli_real_escape_string($conn, $password);
-
-    // $query = "SELECT password FROM customer WHERE username = '$username'";
-
-    // $result = mysqli_query($conn, $query);
-    // $hash = mysqli_fetch_assoc($result)['password'];
-
-    // if (password_verify($password, $hash)){
-    //     $_SESSION['user'] = $username;
-    //     header('Location: dashboard.php');
-    // }else{
-    //     die("Password verification failed");
-    // }
-    
-    // $query = "SELECT * FROM customer WHERE username = '$username'";
-    
-    // if ( $result = mysqli_query($conn, $query) ){
-        //     if (mysqli_num_rows($result) == 0) return true;
-        //     else return false;
-        // }
-    
+function cek_data($username, $password){ 
     require_once "bacalogin.php";
 
     foreach($data1 as $row):
