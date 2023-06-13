6ed0e1237c32e224185fccc509b91c669e40c660
Muhammad Rezzha Riamrizal Arsya <raezz2>
2023-06-14 05:36:25 +0700

    editttt error database

--- booking.php
+++ booking.php
@@ -26,7 +26,7 @@
     <!-- Nav Bar Start -->
     <div class="navbar navbar-expand-lg bg-dark navbar-dark">
         <div class="container-fluid">
-            <a href="index.php" class="navbar-brand"><span>BANGKIT</span></a>
+            <a href="index.html" class="navbar-brand"><span>BANGKIT</span></a>
             <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
                 <span class="navbar-toggler-icon"></span>
             </button>
